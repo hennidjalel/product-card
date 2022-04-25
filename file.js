ed3263b6d8605871b0feb7e665b3006f5562ad1c
Hp <hennidjalel@gmail.com>
2022-04-25 13:27:07 +0200

    first commit

---
+++ file.js
@@ -0,0 +1,31 @@
+var removeCartItemButtons = document.getElementsByClassName("btn-danger")
+console.log(removeCartItemButtons)
+for (i = 0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function (event) {
+        var buttoncliked = event.target
+        buttoncliked.parentElement.parentElement.remove()
+        updateCarteTotal()
+    })
+}
+
+
+function updateCarteTotal() {
+    var cartItemContainer = document.getElementsByClassName("cart-items")[0]
+    var cartRows = cartItemContainer.getElementsByClassName("cart-row")
+    var total = 0;
+
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value
+        console.log(price * quantity)
+        total = total + (price * quantity)
+
+    }
+
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
+
